2ef8fdb368dc3f50b8e4a5fe895d5780ee714391
Jadon Krekos <jkrekos@icloud.com>
2022-05-18 10:17:10 +0800

    Calculator

--- src/main/java/com/KrekosVadam/calculator/Calculator.java
+++ src/main/java/com/KrekosVadam/calculator/Calculator.java
@@ -1,12 +1,28 @@
-package com.KrekosVadam.calculator;
-
 /**
  * Main class for Calculator
  * @author jadonkrekos
  */
 
+package com.KrekosVadam.calculator;
+
 import javax.swing.*;
 
 public class Calculator {
     
+    private static int WINDOW_HEIGHT = 600;
+    private static int WINDOW_LENGTH = 400;
+   
+    JFrame Window;
+    JButton Cnl, Sn, Pct, Div, Mul, 
+            Sub, Add, eq, point, b0, b1, b2, 
+            b3, b4, b5, b6, b7, b8, b9;
+    
+    public Calculator() {
+    
+    }   
+    
+    
+    public static void main(String[] args) {
+        new Calculator();
+    }
 }
